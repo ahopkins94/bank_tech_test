54961ef2a6e35fe6036bcbd3ea297f060c43c6af
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-03 13:25:09 +0000

    Written test for account should update the balance after a withdrawal

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,7 @@ require 'account'
 describe Account do
 
   let(:credit) { double :credit, amount: 100 }
+  let(:debit) { double :debit, amount: -50}
   let(:account) { Account.new }
 
   it 'should have an initial balance of 0' do
@@ -12,6 +13,12 @@ describe Account do
   it 'should update the balance of an account after a deposit' do
     account.update_balance(credit)
     expect(account.balance).to eq 100
+  end
+
+  it 'should update the balance of an account after a withdrawal' do
+    account.update_balance(credit)
+    account.update_balance(debit)
+    expect(account.balance).to eq 50
   end
 
 end
