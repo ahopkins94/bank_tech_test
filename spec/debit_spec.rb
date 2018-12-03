060303febb69ced1f8e3a623ac9037a9553be5e9
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-03 14:28:22 +0000

    Written test for a debit object to have a date

--- spec/debit_spec.rb
+++ spec/debit_spec.rb
@@ -2,10 +2,15 @@ require 'debit'
 
 describe Debit do
 
-  let(:debit) { Debit.new(50) }
+  let(:date) { '12/01/2012' }
+  let(:debit) { Debit.new(50, date) }
 
   it 'should have an amount of 50' do
     expect(debit.amount).to eq -50
+  end
+
+  it 'should have a current date' do
+    expect(debit.date).to eq '12/01/2012'
   end
 
 end
