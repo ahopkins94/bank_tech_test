ca396639eb4ae21a89359e422cb13bcd02aca663
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-03 14:23:54 +0000

    Written test for a credit object to have a date

--- spec/credit_spec.rb
+++ spec/credit_spec.rb
@@ -2,10 +2,15 @@ require 'credit'
 
 describe Credit do
 
-  let(:credit) { Credit.new(100) }
+  let(:date) { '12/01/2012' }
+  let(:credit) { Credit.new(100, date) }
 
   it 'should have an amount of 100' do
     expect(credit.amount).to eq 100
   end
-  
+
+  it 'should have a current date' do
+    expect(credit.date).to eq '12/01/2012'
+  end
+
 end
