4452700ece002f103ba38b197d41fd43eb888e4b
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-03 12:57:00 +0000

    Written test for updating the balance of an account after a deposit

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,10 +2,15 @@ require 'account'
 
 describe Account do
 
-  let(:account) { Account.new }
+  let(:credit) { double :credit, amount: 100 }
+  let(:account) { Account.new(credit) }
 
   it 'should have an initial balance of 0' do
     expect(account.balance).to eq 0
+  end
+
+  it 'should update the balance of an account after a deposit' do
+    expect(account.update_balance).to eq 100
   end
 
 end
