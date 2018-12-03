5ec4048a397770852eff13a94fe984e7bee2228a
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-03 14:26:43 +0000

    Passing test for an credit object has a date

--- lib/credit.rb
+++ lib/credit.rb
@@ -1,9 +1,10 @@
 class Credit
 
-  attr_reader :amount
+  attr_reader :amount, :date
 
-  def initialize(amount)
+  def initialize(amount, date = Time.now.strftime('%d/%m/%Y'))
     @amount = amount
+    @date = date
   end
 
 end
