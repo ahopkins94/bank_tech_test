656a9a5a1c4a3bb697002938272bce3b36385c01
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-04 10:25:46 +0000

    Rewritten class to move responsibility to a transaction class

--- lib/account.rb
+++ lib/account.rb
@@ -1,23 +1,9 @@
-# This class handles the transactions and balance of an account
+# This class handles the balance of an account
 class Account
-  attr_reader :balance, :transactions
+  attr_reader :balance
 
   def initialize
     @balance = 0
-    @transactions = []
   end
 
-  def transaction(action)
-    @balance + action.amount < 0 ? nil : update_balance(action)
-    return @transactions.unshift("#{action.date} || || #{-action.amount}.00 || #{@balance}.00") if action.amount < 0
-
-    @transactions.unshift("#{action.date} || #{action.amount}.00 || || #{@balance}.00")
-  end
-
-  private
-
-  def update_balance(action)
-    @balance += action.amount
-  end
-  
 end
