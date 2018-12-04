1f2bf8d168eb80214b3403c908525e8d99b6287c
Anouska Hopkins <anouskahopkins@gmail.com>
2018-12-04 10:25:23 +0000

    Rewritten test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,45 +1,10 @@
 require 'account'
 
 describe Account do
-  let(:credit) { double :credit, amount: 100, date: '12/01/2012' }
-  let(:debit) { double :debit, amount: -50, date: '13/01/2012' }
   let(:account) { Account.new }
 
   it 'should have an initial balance of 0' do
     expect(account.balance).to eq 0
   end
 
-  it 'should initially have an empty array of transactions' do
-    expect(account.transactions).to eq []
-  end
-
-  describe '.transaction' do
-    before(:each) do
-      account.transaction(credit)
-    end
-
-    it 'should update the balance of an account after a deposit' do
-      expect(account.balance).to eq 100
-    end
-
-    it 'should update the balance of an account after a withdrawal' do
-      account.transaction(debit)
-      expect(account.balance).to eq 50
-    end
-
-    it 'should complete a transaction of a credit' do
-      expect(account.transactions).to eq ['12/01/2012 || 100.00 || || 100.00']
-    end
-
-    it 'should complete a transaction of a debit' do
-      account.transaction(debit)
-      expect(account.transactions).to eq ['13/01/2012 || || 50.00 || 50.00', '12/01/2012 || 100.00 || || 100.00']
-    end
-
-    it 'should not complete transaction of a debit if the balance would return negative' do
-      2.times { account.transaction(debit) }
-      account.transaction(debit)
-      expect(account.balance).to eq 0
-    end
-  end
 end
